aacd702d5bb245ce8b01c31c78c8c09a4351c8b4
James <Intellemerc@users.noreply.github.com>
2016-12-25 19:13:12 -0500

    add back in the react logo

--- ReactWepApi/src/App.js
+++ ReactWepApi/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import logo from './logo.svg';
 class App extends React.Component {
     render() {
         return (React.createElement("div", { className: "App" },
             React.createElement("div", { className: "App-header" },
-                React.createElement("h2", null, "Welcome to Reacct")),
+                React.createElement("h2", null, "Welcome to React"),
+                React.createElement("img", { src: logo, className: "App-logo", alt: "logo" })),
             React.createElement("p", { className: "App-intro" },
                 "Edit ",
                 React.createElement("code", null, "src/App.js"),
--- ReactWepApi/src/App.tsx
+++ ReactWepApi/src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import logo from './logo.svg';
+
 
 
 
@@ -8,11 +10,11 @@ class App extends React.Component<{}, {}> {
         return (
             <div className="App"> 
                 <div className="App-header">
-                    <h2>Welcome to Reacct</h2>
+                    <h2>Welcome to React</h2>
+                    <img src={logo} className="App-logo" alt="logo" />
                 </div>
                 <p className="App-intro">
-
-                    Edit <code>src/App.js</code> and save to reload.
+                   Edit <code>src/App.js</code> and save to reload.
                 </p>
             </div>
 
