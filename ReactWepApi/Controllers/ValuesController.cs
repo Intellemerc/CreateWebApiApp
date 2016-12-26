6e33f94979f782a1de13f98cfe1547987e4d775e
James <Intellemerc@users.noreply.github.com>
2016-12-25 20:27:16 -0500

    Hook up react build to webapi with url rewrite

--- ReactWepApi/Controllers/ValuesController.cs
+++ ReactWepApi/Controllers/ValuesController.cs
@@ -7,7 +7,7 @@ using System.Web.Http;
 
 namespace ReactWepApi.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class ValuesController : ApiController
     {
         // GET api/values
