0a374d3e2b82748188b68e3007f874cf01d9f02a
James <Intellemerc@users.noreply.github.com>
2016-12-25 19:53:20 -0500

    Fix logo import in typescript

--- ReactWepApi/src/App.js
+++ ReactWepApi/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import logo from './logo.svg';
+const logo = require('./logo.svg');
 class App extends React.Component {
     render() {
         return (React.createElement("div", { className: "App" },
--- ReactWepApi/src/App.tsx
+++ ReactWepApi/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import logo from './logo.svg';
+const logo = require('./logo.svg');
 
 
 
