0e46605fb06963088918330f774aca12e4dc5b38
James <Intellemerc@users.noreply.github.com>
2017-01-03 00:40:28 -0500

    Add CORS dependency

--- ReactWebApi/App_Start/WebApiConfig.cs
+++ ReactWebApi/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
 
 namespace ReactWebApi
 {
@@ -25,6 +26,11 @@ namespace ReactWebApi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+#if DEBUG
+            var cors = new EnableCorsAttribute("http://localhost:3000", "*", "*");
+            config.EnableCors(cors);
+#endif
         }
     }
 }
